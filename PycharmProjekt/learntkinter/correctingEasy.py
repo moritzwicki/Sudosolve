49759258c0b804058d41abebfba2e804d65dcca6
Moritz Wicki <wickimoritz09@gmail.com>
2019-12-19 08:46:54 +0100

    Push 19.12 Ich kann das erste Sudoku korrigieren

---
+++ PycharmProjekt/learntkinter/correctingEasy.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+Gewinnfenster = tk.Tk()
+board = ['7', '1', '8', '3', '4', '9', '6', '5', '2', '9', '5', '2', '1', '6', '7', '4', '8', '3', '4', '6', '3', '5',
+         '2', '8', '7', '9', '1', '5', '7', '4', '2', '1', '3', '9', '6', '8', '8', '3', '9', '7', '5', '6', '1', '2',
+         '4', '6', '2', '1', '8', '9', '4', '5', '3', '7', '2', '4', '7', '9', '3', '5', '8', '1', '6', '3', '9', '6',
+         '4', '8', '1', '2', '7', '5', '1', '8', '5', '6', '7', '2', '3', '4', '9']
+
+
+def geteingaben(entries):
+    countCorrect = 0
+    countFaults = 0
+    for i in range(len(board)):
+        correcting = entries[i].get()
+        if correcting == board[i]:
+            countCorrect += 1
+        else:
+            countFaults += 1
+
+        if countCorrect == 81:
+            tk.Label(Gewinnfenster, text="Sie haben keine gemacht").pack()
--- PycharmProjekt/learntkinter/firstgui.py
+++ PycharmProjekt/learntkinter/firstgui.py
@@ -1,15 +1,13 @@
 import array
-
+import sudokugenerator as sg
 import tkinter as tk
-import tkMessageBox
-from Tkinter import IntVar, Entry
+import correctingEasy as cor
 
-
+entries = {}
 
 
 def drawGrid():
     root = tk.Tk()
-    entries = {}
     tableheight = 9
     tablewidth = 9
     counter = 0
@@ -18,9 +16,60 @@ def drawGrid():
             entries[counter] = tk.Entry(root, width=2)
             entries[counter].grid(row=row, column=column)
             counter += 1
-    print entries
-    entries[11].insert(0,"2")
-    entries[11].insert(0,"3")
+    sg.createEasy(entries)
+    entries[0].configure(bg="azure")
+    entries[1].configure(bg="azure")
+    entries[2].configure(bg="azure")
+    entries[6].configure(bg="azure")
+    entries[7].configure(bg="azure")
+    entries[8].configure(bg="azure")
+    entries[9].configure(bg="azure")
+    entries[10].configure(bg="azure")
+    entries[11].configure(bg="azure")
+    entries[15].configure(bg="azure")
+    entries[16].configure(bg="azure")
+    entries[17].configure(bg="azure")
+    entries[18].configure(bg="azure")
+    entries[19].configure(bg="azure")
+    entries[20].configure(bg="azure")
+    entries[24].configure(bg="azure")
+    entries[25].configure(bg="azure")
+    entries[26].configure(bg="azure")
+    entries[30].configure(bg="azure")
+    entries[31].configure(bg="azure")
+    entries[32].configure(bg="azure")
+    entries[39].configure(bg="azure")
+    entries[40].configure(bg="azure")
+    entries[41].configure(bg="azure")
+    entries[48].configure(bg="azure")
+    entries[49].configure(bg="azure")
+    entries[50].configure(bg="azure")
+    entries[54].configure(bg="azure")
+    entries[55].configure(bg="azure")
+    entries[56].configure(bg="azure")
+    entries[60].configure(bg="azure")
+    entries[61].configure(bg="azure")
+    entries[62].configure(bg="azure")
+    entries[63].configure(bg="azure")
+    entries[64].configure(bg="azure")
+    entries[65].configure(bg="azure")
+    entries[69].configure(bg="azure")
+    entries[70].configure(bg="azure")
+    entries[71].configure(bg="azure")
+    entries[72].configure(bg="azure")
+    entries[73].configure(bg="azure")
+    entries[74].configure(bg="azure")
+    entries[78].configure(bg="azure")
+    entries[79].configure(bg="azure")
+    entries[80].configure(bg="azure")
+
+
+    submitButton = tk.Button(root, text="Correcting", command=lambda: cor.geteingaben(entries))
+    submitButton.grid(row=10, column=10)
+
+    cor.geteingaben(entries)
+
+
 win = tk.Tk()
 win.geometry('1100x700')
 win.title("First Gui")
@@ -31,14 +80,5 @@ tk.Button(win, text="leicht", command=lambda: drawGrid()).pack()
 tk.Button(win, text="Mittel").pack()
 tk.Button(win, text="Schwierig").pack()
 tk.Button(win, text="Extrem").pack()
-
-
-
-
-#def EntryOutput(rootIdentyfier, windowIdentifier, outputValue):
-   # return rootIdentyfier.StringVar(windowIdentifier, value=outputValue)
-
-
-#test = Entry(win, textvariable=EntryOutput(tk, win, "Work mf work")).pack()
 
 win.mainloop()
