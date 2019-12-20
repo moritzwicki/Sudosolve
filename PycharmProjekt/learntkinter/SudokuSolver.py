2fd4e0a2a7a7947a536e4732422888608cf835ff
Moritz Wicki <wickimoritz09@gmail.com>
2019-12-20 16:20:57 +0100

    Push 20.12 Das Sudoku funktionier. Der Solver noch nicht

--- PycharmProjekt/learntkinter/SudokuSolver.py
+++ PycharmProjekt/learntkinter/SudokuSolver.py
@@ -1,2 +1,56 @@
-def SudokuSolving(entries):
-    print "hello World"
+from StdSuites import numbers
+from Tkconstants import END
+
+
+"""def SudokuSolving(entries):
+    for i in range(len(entries)):
+        solving = entries[i].get()
+        if solving == '' or ' ' or 'a' or 'b' or 'c' or 'd' or 'e' or 'f' or 'g' or 'h' or 'i' or 'j' or 'k' or 'l' or 'm' or 'n' or 'o' or 'p' or 'c' or 'q' or 'r' or 's' or 't' or 'u' or 'v' or 'w' or 'x' or 'y' or 'z' or '.' or ',' or '-':
+            entries[i].delete(0, END)
+
+def oneToNine(num1, num2, num3, num4, num5, num6, num7, num8, num9):
+    nums = [num1, num2, num3, num4, num5, num6, num7, num8, num9]
+    one = False
+    two = False
+    three = False
+    four = False
+    five = False
+    six = False
+    seven = False
+    eight = False
+    nine = False
+    for x in range(10):
+        if nums[x] == '1':
+            one = True
+        if nums[x] == '2':
+            two = True
+        if nums[x] == '3':
+            three = True
+        if nums[x] == '4':
+            four = True
+        if nums[x] == '5':
+            five = True
+        if nums[x] == '6':
+            six = True
+        if nums[x] == '7':
+            seven = True
+        if nums[x] == '8':
+            eight = True
+        if nums[x] == '9':
+            nine = True
+    if one == True:
+        if two == True:
+            if three == True:
+                if four == True:
+                    if five == True:
+                        if six == True:
+                            if seven == True:
+                                if eight == True:
+                                    if nine == True:
+                                        return True
+    return False
+
+
+if oneToNine(myArray[0], myArray[1],) == False:
+    return false
+return True"""
